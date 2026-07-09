3ce0e68f363f88bbc1ce8873f31b99b3cd01c7eb
ageeva_veronika <ageeva.veronika80@mail.ru>
2026-07-09 16:16:53 +0000

    71

--- .laborantum/src/models/feedforward/autoencoder.py
+++ .laborantum/src/models/feedforward/autoencoder.py
@@ -1,12 +1,12 @@
 import torch
 
+
 class Autoencoder(torch.nn.Module):
     def __init__(
-            self,
-            channels,
-            activation=torch.nn.ReLU):
-        ...
-        ## YOUR CODE HERE
+        self,
+        channels,
+        activation=torch.nn.ReLU
+    ):
         super().__init__()
 
         encoder_layers = []
@@ -18,60 +18,65 @@ class Autoencoder(torch.nn.Module):
         self.encoder = torch.nn.Sequential(*encoder_layers)
 
         decoder_layers = []
-        channels = channels[::-1]
-        for index in range(len(channels) - 1):
-            decoder_layers.append(torch.nn.Linear(channels[index], channels[index + 1]))
+        decoder_channels = channels[::-1]
+        for index in range(len(decoder_channels) - 1):
+            decoder_layers.append(torch.nn.Linear(decoder_channels[index], decoder_channels[index + 1]))
             decoder_layers.append(activation())
         decoder_layers.pop()
 
         self.decoder = torch.nn.Sequential(*decoder_layers)
 
-    def __call__(self, signal):
+    def __forward_kernel(self, signal):
         input_shape = signal.shape
-        res = signal
-        ## YOUR CODE HERE
-        res = res.reshape([res.shape[0], -1])
+
+        res = signal.reshape(signal.shape[0], -1)
         res = self.encoder(res)
         res = self.decoder(res)
         res = res.reshape(input_shape)
+
         return res
 
+    def forward(self, batch):
+        if isinstance(batch, dict):
+            signal = batch['data']['image']
+            reconstruction = self.__forward_kernel(signal)
+
+            batch['signals'] = {
+                'reconstruction': reconstruction
+            }
+
+            return batch
+
+        return self.__forward_kernel(batch)
 
 class Sampler(torch.nn.Module):
     def __init__(self, channels):
-        ...
-        ## YOUR CODE HERE
         super().__init__()
+
         self.mu_regressor = torch.nn.Linear(channels, channels)
         self.logvar_regressor = torch.nn.Linear(channels, channels)
 
-
-    def __call__(self, signal):
-        res = signal
-        mu = signal
-        sigma = signal
-
-        ## YOUR CODE HERE
+    def forward(self, signal):
         mu = self.mu_regressor(signal)
         logvar = self.logvar_regressor(signal)
 
-        sigma = (logvar / 2).exp()
+        sigma = torch.exp(logvar / 2)
 
         if self.training:
             noise = torch.randn_like(mu)
-            res = noise * sigma + mu
+            res = mu + noise * sigma
         else:
             res = mu
+
         return res, mu, sigma
 
 
 class VAE(torch.nn.Module):
     def __init__(
-            self,
-            channels,
-            activation=torch.nn.ReLU):
-        ...
-        ## YOUR CODE HERE
+        self,
+        channels,
+        activation=torch.nn.ReLU
+    ):
         super().__init__()
 
         encoder_layers = []
@@ -84,21 +89,21 @@ class VAE(torch.nn.Module):
         self.sampler = Sampler(channels[-1])
 
         decoder_layers = []
-        channels = channels[::-1]
-        for index in range(len(channels) - 1):
-            decoder_layers.append(torch.nn.Linear(channels[index], channels[index + 1]))
+        decoder_channels = channels[::-1]
+        for index in range(len(decoder_channels) - 1):
+            decoder_layers.append(torch.nn.Linear(decoder_channels[index], decoder_channels[index + 1]))
             decoder_layers.append(activation())
         decoder_layers.pop()
 
         self.decoder = torch.nn.Sequential(*decoder_layers)
 
-    def __call__(self, signal):
+    def forward(self, signal):
         input_shape = signal.shape
-        res = signal
-        ## YOUR CODE HERE
-        res = res.reshape([res.shape[0], -1])
+
+        res = signal.reshape([signal.shape[0], -1])
         res = self.encoder(res)
         res, mu, sigma = self.sampler(res)
         res = self.decoder(res)
         res = res.reshape(input_shape)
+
         return res, mu, sigma
